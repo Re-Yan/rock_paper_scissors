dea12d1a17a8eef72bcfd820d11c71c4defef56d
Re-Yan <reannub@gmail.com>
2023-02-12 21:55:23 +0800

    add playRound function
    playRound will check which side will win the game

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 'strict mode'
 
-console.log('test');
+const playerSelection = 'paper';
 
-const getComputerChoice = function() {
+let getComputerChoice = function() {
     let computerChoice;
     let randnum = Math.floor((Math.random() * (4 - 1)) + 1);
 
     if (randnum === 1) {
-        return computerChoice = 'rock';
+        return 'rock';
     } else if (randnum === 2) {
-        return computerChoice = 'paper';
+        return 'paper';
     } else {
-        return computerChoice = 'scissors';
+        return 'scissors';
     };
     
 };
+
+let computerChoice = getComputerChoice();
+console.log(`computer choice: ${computerChoice}`);
+
+const playRound = function (playerSelection, computerChoice) {
+
+    if (playerSelection === 'rock' && computerChoice === 'rock') return 'Draw';
+    else if (playerSelection === 'rock' && computerChoice === 'paper') return 'You Lose';
+    else if (playerSelection === 'rock' && computerChoice === 'scissors') return 'You Win';
+
+    if (playerSelection === 'paper' && computerChoice === 'rock') return 'You Win';
+    else if (playerSelection === 'paper' && computerChoice === 'paper') return 'Draw';
+    else if (playerSelection === 'paper' && computerChoice === 'scissors') return 'You Lose';
+
+    if (playerSelection === 'scissors' && computerChoice === 'rock') return 'You Lose';
+    else if (playerSelection === 'scissors' && computerChoice === 'paper') return 'You Win';
+    else if (playerSelection === 'scissors' && computerChoice === 'scissors') return 'Draw';
+};
+
+const gameResult = playRound(`${playerSelection}`, `${computerChoice}`);
+console.log(gameResult);
