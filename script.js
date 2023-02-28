239188fa839287639276c64c8f460d0eb61d26f5
Re-Yan <reannub@gmail.com>
2023-02-28 22:32:21 +0800

    Fix Logic in Script.js file
        - adjust styling for all elements in style.css file
        - add necessary divs for proper structure in index.html file

--- script.js
+++ script.js
@@ -1,53 +1,154 @@
-'strict mode'
+"strict mode";
 
 let playerSelection, computerChoice, promptResult;
+let scores = [0, 0];
+let gameActive = true;
+// Function Declarations
 
-    // Function Declarations
-const getComputerChoice = function() {
-    let computerChoice;
-    let randnum = Math.floor((Math.random() * (4 - 1)) + 1);
+const getComputerChoice = function () {
+  let randnum = Math.floor(Math.random() * (4 - 1) + 1);
 
-    if (randnum === 1) {
-        return 'rock';
-    } else if (randnum === 2) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    };
+  if (randnum === 1) {
+    return (computerChoice = "rock");
+  } else if (randnum === 2) {
+    return (computerChoice = "paper");
+  } else {
+    return (computerChoice = "scissors");
+  }
 };
 
 const playRound = function (playerSelection, computerChoice) {
+  if (playerSelection === "rock" && computerChoice === "rock") {
+    mainDescription.textContent = "DRAW";
+    subDescription.textContent = "";
+  } else if (playerSelection === "rock" && computerChoice === "paper") {
+    mainDescription.textContent = "COMPUTER WIN";
+    subDescription.textContent = "";
+    scores[1] += 1;
+    computerScore.textContent = scores[1];
+  } else if (playerSelection === "rock" && computerChoice === "scissors") {
+    mainDescription.textContent = "PLAYER WIN";
+    subDescription.textContent = "";
+    scores[0] += 1;
+    playerScore.textContent = scores[0];
+  }
 
-    if (playerSelection === 'rock' && computerChoice === 'rock') return 'Draw';
-    else if (playerSelection === 'rock' && computerChoice === 'paper') return 'You Lose';
-    else if (playerSelection === 'rock' && computerChoice === 'scissors') return 'You Win';
+  if (playerSelection === "paper" && computerChoice === "rock") {
+    mainDescription.textContent = "PLAYER WIN";
+    subDescription.textContent = "";
+    scores[0] += 1;
+    playerScore.textContent = scores[0];
+  } else if (playerSelection === "paper" && computerChoice === "paper") {
+    mainDescription.textContent = "DRAW";
+    subDescription.textContent = "";
+  } else if (playerSelection === "paper" && computerChoice === "scissors") {
+    mainDescription.textContent = "COMPUTER WIN";
+    subDescription.textContent = "";
+    scores[1] += 1;
+    computerScore.textContent = scores[1];
+  }
 
-    if (playerSelection === 'paper' && computerChoice === 'rock') return 'You Win';
-    else if (playerSelection === 'paper' && computerChoice === 'paper') return 'Draw';
-    else if (playerSelection === 'paper' && computerChoice === 'scissors') return 'You Lose';
-
-    if (playerSelection === 'scissors' && computerChoice === 'rock') return 'You Lose';
-    else if (playerSelection === 'scissors' && computerChoice === 'paper') return 'You Win';
-    else if (playerSelection === 'scissors' && computerChoice === 'scissors') return 'Draw';
+  if (playerSelection === "scissors" && computerChoice === "rock") {
+    mainDescription.textContent = "COMPUTER WIN";
+    subDescription.textContent = "";
+    scores[1] += 1;
+    computerScore.textContent = scores[1];
+  } else if (playerSelection === "scissors" && computerChoice === "paper") {
+    mainDescription.textContent = "PLAYER WIN";
+    subDescription.textContent = "";
+    scores[0] += 1;
+    playerScore.textContent = scores[0];
+  } else if (playerSelection === "scissors" && computerChoice === "scissors") {
+    mainDescription.textContent = "DRAW";
+    subDescription.textContent = "";
+  }
 };
 
-const game = function() {
-    for (let i = 0; i < 5; i++) {
-        computerChoice = getComputerChoice();
-        promptResult = prompt('Choose: Rock, Paper or Scissors');
-        playerSelection = promptResult.toLowerCase();
-        console.log(`computer choice: ${computerChoice}`);
-        console.log(playRound(`${playerSelection}`, `${computerChoice}`));
-    }
-}
+const computerPickDescription = () => {
+  if (computerChoice === "rock") {
+    compSelectionName.textContent = "The computer played ROCK";
+  } else if (computerChoice === "paper") {
+    compSelectionName.textContent = "The computer played PAPER";
+  } else {
+    compSelectionName.textContent = "The computer played SCISSORS";
+  }
+};
 
-computerChoice = getComputerChoice();
-console.log(`COMPUTER CHOICE: ${computerChoice}`);
+const scoreChecker = () => {
+  if (scores[0] >= 5) {
+    modal.classList.remove("hidden");
+    winnerMessage.textContent = "YOU WON!";
+    gameActive = false;
+  } else if (scores[1] >= 5) {
+    modal.classList.remove("hidden");
+    winnerMessage.textContent = "YOU LOST!";
+    gameActive = false;
+  }
+};
+
+const pictureChanger = () => {
+  // Changes the picture to the corresponding computer pick
+  if (computerChoice === "rock") {
+    computerPick.src = "./pictures/rock.jpeg";
+  } else if (computerChoice === "paper") {
+    computerPick.src = "./pictures/paper.jpeg";
+  } else {
+    computerPick.src = "./pictures/scissors.jpeg";
+  }
+};
+
+// DOM elements
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const computerPick = document.querySelector("#computerPick");
+const mainDescription = document.querySelector("#main-description");
+const subDescription = document.querySelector("#sub-description");
+const compSelectionName = document.getElementById("compSelectionName");
+const playerScore = document.querySelector("#current-score-0");
+const computerScore = document.querySelector("#current-score-1");
+const modal = document.querySelector(".modal");
+const winnerMessage = document.querySelector(".winner");
+const reset = document.querySelector(".reset");
 
 // Events for each button
-const rockBtn = document.querySelector('.rockButton');
-rockBtn.addEventListener('click', ()  => console.log(playRound('rock', `${computerChoice}`)));
-const paperBtn = document.querySelector('.paperButton');
-paperBtn.addEventListener('click', () => console.log(playRound('paper', `${computerChoice}`)));
-const scissorsBtn = document.querySelector('.scissorsButton');
-scissorsBtn.addEventListener('click', () => console.log(playRound('scissors', `${computerChoice}`)));
+
+rockBtn.addEventListener("click", () => {
+  if (gameActive) {
+    computerChoice = getComputerChoice();
+    playRound("rock", `${computerChoice}`);
+    computerPickDescription();
+    pictureChanger();
+    scoreChecker();
+  }
+});
+paperBtn.addEventListener("click", () => {
+  if (gameActive) {
+    computerChoice = getComputerChoice();
+    playRound("paper", `${computerChoice}`);
+    computerPickDescription();
+    pictureChanger();
+    scoreChecker();
+  }
+});
+scissorsBtn.addEventListener("click", () => {
+  if (gameActive) {
+    computerChoice = getComputerChoice();
+    playRound("scissors", `${computerChoice}`);
+    computerPickDescription();
+    pictureChanger();
+    scoreChecker();
+  }
+});
+
+reset.addEventListener("click", function () {
+  scores = [0, 0];
+  playerScore.textContent = scores[0];
+  computerScore.textContent = scores[1];
+  computerPick.src = "./pictures/QuestionMark.PNG";
+  modal.classList.add("hidden");
+  mainDescription.textContent = "Ready to Play?";
+  subDescription.textContent = "Choose Your Pick";
+  compSelectionName.textContent = " ";
+  gameActive = true;
+});
